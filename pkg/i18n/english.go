7640469693adf0519e78a5963d1b840ba69935aa
Stefan Haller <stefan@haller-berlin.de>
2023-07-24 12:29:42 +0200

    Allow force-tagging if tag exists

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -357,6 +357,8 @@ type TranslationSet struct {
 	PushTag                             string
 	CreateTag                           string
 	CreatingTag                         string
+	ForceTag                            string
+	ForceTagPrompt                      string
 	FetchRemote                         string
 	FetchingRemoteStatus                string
 	CheckoutCommit                      string
@@ -1052,6 +1054,8 @@ func EnglishTranslationSet() TranslationSet {
 		PushTag:                             "Push tag",
 		CreateTag:                           "Create tag",
 		CreatingTag:                         "Creating tag",
+		ForceTag:                            "Force Tag",
+		ForceTagPrompt:                      "The tag '{{.tagName}}' exists already. Press 'esc' to cancel, or 'enter' to overwrite.",
 		FetchRemote:                         "Fetch remote",
 		FetchingRemoteStatus:                "Fetching remote",
 		CheckoutCommit:                      "Checkout commit",
